1c95853c9d922eb3c20b75baf166c0c9f1894a1e
hussien mohamad <hussien.hesham04@eng-st.cu.edu.eg>
2025-12-14 01:27:47 +0200

    Update pc.v

--- pc/pc.v
+++ pc/pc.v
@@ -1,19 +1,17 @@
-module pc (
-input wire rst ,
-input wire [7:0] pc_in ,
-output reg [7:0] pc_out
+module pc_reg ( 
+    input clk,
+	input rst,
+	input enable , // it is complement
+    input [7:0] pc_in,
+    output reg [7:0] pc_out
 );
-
-always @(*)
-	begin
-		if(~rst)
-			begin
-				pc_out = 'd0 ;
-			end
-		else
-			begin
-				pc_out = pc_in ;
-			end
-	end
-	
+    always @(posedge clk or negedge rst) begin
+        if(~rst)
+            pc_out <= 0;
+        else 
+		begin
+            if(~enable)
+				pc_out <= pc_in;
+        end
+    end
 endmodule
