718b6a5ed4673d6110ccdc6164034115066e73ee
hussien mohamad <hussien.hesham04@eng-st.cu.edu.eg>
2025-12-14 01:59:05 +0200

    Update pc.v

--- pc/pc.v
+++ pc/pc.v
@@ -1,18 +1,12 @@
 module pc_reg ( 
     input clk,
-	input rst,
 	input enable , 
     input [7:0] pc_in,
     output reg [7:0] pc_out
 );
-    always @(posedge clk or negedge rst) 
-		begin
-        if(~rst)
-            pc_out <= 0;
-        else 
+	always @(posedge clk ) 
 		begin
             if(enable)
 				pc_out <= pc_in;
         end
-    end
 endmodule
