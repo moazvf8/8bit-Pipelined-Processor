09910559db0d3e6092d8c2975d8ab612460dc22e
shahd-elsehimy <shahdmahmoud00000@gmail.com>
2025-12-14 02:30:22 +0200

    Solve compile error

--- adder.v
+++ adder.v
@@ -1,7 +1,7 @@
 module adder_8bit (
     input wire [7:0] a,      // 8-bit Input A
     input wire [7:0] b,      // 8-bit Input B
-    output wire [7:0] sum,   // 8-bit Sum
+    output wire [7:0] sum   // 8-bit Sum
 );
 
     assign sum = a + b;
