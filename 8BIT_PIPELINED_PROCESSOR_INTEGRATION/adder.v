80ad16dd0d3fe8a7214aa4264f5362c266dd662a
Farah-Elsaay <farahelsaay1@gmail.com>
2026-02-06 00:22:28 +0200

    Delete 8BIT_PIPELINED_PROCESSOR_INTEGRATION directory

--- 8BIT_PIPELINED_PROCESSOR_INTEGRATION/adder.v
+++
@@ -1,9 +0,0 @@
-module adder (
-    input wire [7:0] a,      // 8-bit Input A
-    input wire [7:0] b,      // 8-bit Input B
-    output wire [7:0] sum   // 8-bit Sum
-);
-
-    assign sum = a + b;
-
-endmodule
